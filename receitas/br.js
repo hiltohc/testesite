dc68f8842dfb896194ad1c234a260e56a9d3cab7
hiltohc <105225792+hiltohc@users.noreply.github.com>
2022-06-05 06:52:46 -0300

    Add files via upload

---
+++ receitas/br.js
@@ -0,0 +1,25 @@
+const modalOverlay = document.querySelector('.modal-overlay')
+const cards = document.querySelectorAll('.card')
+
+for (let card of cards){
+    card.addEventListener ("click" , function (){
+        const imageId = card.getAttribute("id")
+        const title = card.querySelector('.title_recipe').innerText // atribui o texto que contém 
+        const author = card.querySelector('.card_chef').innerText
+
+        modalOverlay.classList.add('active')
+        modalOverlay.querySelector('img').src = `/images/brasileiras/${imageId}`
+        modalOverlay.querySelector('.title-modal').innerHTML = `${title}`
+        modalOverlay.querySelector('.chef-modal').innerHTML = `${author}`
+        modalOverlay.querySelector('').innerHTML = `${imageId}`
+
+        /* fazer para ingredientes tb*/
+    })
+}
+
+const closeModal = document.querySelector('.close-modal')
+
+closeModal.addEventListener("click", function (){
+    modalOverlay.classList.remove('active') // remove classe active p/ fechar modal
+    modalOverlay.querySelector('img').src = ""
+})
